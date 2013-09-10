bd19bf28f2ecd86d885d85909e39727b23430988
vagrant <vagrant@precise32.(none)>
2013-09-10 21:42:00 +0000

    copy home bash profile

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 cp vim_config/vimrc ~/.vimrc
 cp bash_config/profile ~/.profile
 cp bash_config/bash_profile ~/.bash_profile
+cp bash_config/home_bash_profile /home/vagrant/.bash_profile
 cp gem_config/gemrc ~/.gemrc
 
 
