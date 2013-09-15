09ef3a4c91faa1bc892295adbee5a2bf1edbc318
Duncan Butler <duncan.butler1@btinternet.com>
2013-09-15 20:09:01 +0000

    updated config and started creation of environmet file

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cp bash_config/bash_profile ~/.bash_profile
 cp bash_config/home_bash_profile /home/vagrant/.bash_profile
 cp gem_config/gemrc ~/.gemrc
 cp tmux_config/tmux.conf ~/.tmux.conf
+cp tmux_config/development_environment ~/dev_env
 
 sudo cp keyboard_config/keyboard ../../../etc/default/keyboard
 
