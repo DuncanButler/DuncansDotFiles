cd44825084df6fd0d7aa2c35e20695a99a3d6e58
Duncan Butler <duncan.butler1@btinternet.com>
2013-09-15 01:46:12 +0000

    updated tmux config

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ cp bash_config/bash_profile ~/.bash_profile
 cp bash_config/home_bash_profile /home/vagrant/.bash_profile
 cp gem_config/gemrc ~/.gemrc
 cp tmux_config/tmux.conf ~/.tmux.conf
+
 sudo cp keyboard_config/keyboard ../../../etc/default/keyboard
 
