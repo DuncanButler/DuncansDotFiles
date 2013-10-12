53741350693261e1c0b4b7d606a1d3a5cb25c06f
Duncan Butler <duncan.butler1@btinternet.com>
2013-10-12 17:10:02 +0000

    added ruby environment install and ruby build

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
-# move vim configuration
-cp vim_config/vimrc ~/.vimrc
+# install ruby environment
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+
+# bash configuration
 cp bash_config/profile ~/.profile
 cp bash_config/bash_profile ~/.bash_profile
 cp bash_config/home_bash_profile /home/vagrant/.bash_profile
+
+# add rbenv to path and init
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+
+# install ruby build
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# move vim configuration
+cp vim_config/vimrc ~/.vimrc
 cp gem_config/gemrc ~/.gemrc
 cp tmux_config/tmux.conf ~/.tmux.conf
 
